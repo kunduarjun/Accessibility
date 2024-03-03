fdff1c11608859fd1b1e3c6f9fc101323ccc5478
Arjun Kundu <akundu8149@upei.ca>
2024-03-02 20:53:26 -0400

    Added some information to determine Latitude and Longitude.

--- Get_Current_Location.py
+++ Get_Current_Location.py
@@ -12,12 +12,16 @@ def get_user_location():
             # print(f"Region: {location.region}")
             print(f"Country: {location.country}")
             print(f"Latitude: {location.latlng[0]}, Longitude: {location.latlng[1]}")
+            return location.city, location.country, location.latlng[0], location.latlng[1]
         else:
             print("Unable to determine location based on IP address.")
+            return None
 
     except Exception as e:
         print(f"An error occurred: {e}")
+        return None
 
 
 # Call the function to get the user's location
-get_user_location()
+city, country, latitude, longitude = get_user_location()
+print(f'City: {city}, Country: {country}, Latitude: {latitude}, Longitude: {longitude}')
--- Input_Location.py
+++ Input_Location.py
@@ -10,12 +10,16 @@ def get_user_coordinates(address):
         if location:
             latitude, longitude = location.latitude, location.longitude
             print(f"Latitude: {latitude}, Longitude: {longitude}")
+            return latitude, longitude
         else:
             print("Unable to determine coordinates for the provided address.")
+            return None, None
 
     except Exception as e:
         print(f"An error occurred: {e}")
+        return None, None
 
 
 # Call the function to get the user's coordinates
-get_user_coordinates("364 University Ave")
+latitude_1, longitude_1 = get_user_coordinates("364 University Ave")
+# Test: print(f"Latitude: {latitude_1}, Longitude: {longitude_1}")
